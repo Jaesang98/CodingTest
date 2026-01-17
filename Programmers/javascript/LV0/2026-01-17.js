b570019c69a775d0937039d0d20f891b9b930032
jsnam <skawotkd12@naver.com>
2026-01-18 03:06:05 +0900

    2026-01-18 맛보기 자 다시 열심히 해보자..!

---
+++ Programmers/javascript/LV0/2026-01-17.js
@@ -0,0 +1,44 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+function solution(num1, num2) {
+  return num1 * num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+function solution(num1, num2) {
+  return num1 - num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
+function solution(age) {
+  return 2023 - age;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+function solution(num1, num2) {
+  return Math.floor((num1 / num2) * 1000);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120810
+function solution(num1, num2) {
+  return num1 % num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805
+function solution(num1, num2) {
+  return Math.floor(num1 / num2);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+function solution(num1, num2) {
+  return num1 + num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120898
+function solution(message) {
+  return message.length * 2;
+}
